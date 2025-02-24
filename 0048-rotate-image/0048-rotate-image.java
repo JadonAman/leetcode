edfa7fc1c043e6be424afe2c28e4fadc6cb89b7e
Aman Jadon <106210768+JadonAman@users.noreply.github.com>
2025-02-24 22:16:12 +0530

    Time: 0 ms (100%), Space: 42.5 MB (24.77%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -4,15 +4,18 @@ class Solution {
         int m = matrix[0].length;
         int[][] arr = new int[n][m];
         for(int i=0; i<n; i++){
-            for(int j=0; j< m ; j++){
-                arr[j][n-1-i]=matrix[i][j];
+            for(int j=i+1; j< m ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
         for(int i=0; i<n; i++){
-            for(int j=0; j< m ; j++){
-                matrix[i][j]=arr[i][j];
+            for(int j=0; j<m/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
             }
         }
-        
     }
 }
