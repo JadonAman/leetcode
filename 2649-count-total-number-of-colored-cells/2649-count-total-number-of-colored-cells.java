eee4ec8c3370c5af8b8f3b3c2729e406cb1956a1
Aman Jadon <106210768+JadonAman@users.noreply.github.com>
2025-03-05 11:22:15 +0530

    Time: 0 ms (100%), Space: 40.4 MB (59.02%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,10 +1,7 @@
 class Solution {
-    public long coloredCells(int n) {
-        if(n==1) return 1;
-        long sum =1;
-        for(int i=2; i<=n; i++){
-            sum += 4*(i-1);
-        }
-        return sum;
+    public long coloredCells(int p) {
+        long n=p;
+       long sum = 1+(4*((n*(n-1))/2));
+       return sum;
     }
 }
